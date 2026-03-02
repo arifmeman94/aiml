3c2fe00074cdc0b7636f9180d9831831f6320676
arifmeman94 <arifmeman94@gmail.com>
2026-03-02 05:30:25 +0000

    Python

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,54 +1,40 @@
+
 # To learn more about how to use Nix to configure your environment
 # see: https://developers.google.com/idx/guides/customize-idx-env
 { pkgs, ... }: {
   # Which nixpkgs channel to use.
-  channel = "stable-24.05"; # or "unstable"
-  # Use https://search.nixos.org/packages to find packages
+  channel = "stable-25.05"; # or "unstable"
+
+  # Use https://search.nixos.org/packages to find packages.
+  # We are installing python and common data science libraries.
+  # It's best practice to manage these with Nix to ensure a reproducible environment.
   packages = [
-    # pkgs.go
-    # pkgs.python311
-    # pkgs.python311Packages.pip
-    # pkgs.nodejs_20
-    # pkgs.nodePackages.nodemon
+    pkgs.python313Full
+    pkgs.python313Packages.pip # pip is still useful for other packages
+    pkgs.python313Packages.scikit-learn
+    pkgs.python313Packages.pandas
+    pkgs.python313Packages.numpy
+    pkgs.python313Packages.jupyter
+    pkgs.tenv
+    pkgs.terraform
+    pkgs.xvfb-run
   ];
+
   # Sets environment variables in the workspace
-  env = {};
+  env = {
+    TENV_AUTO_INSTALL = "true";
+  };
+
   idx = {
-    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
+    # Search for extensions on https://open-vsx.org/
     extensions = [
-      # "vscodevim.vim"
+      "ms-python.python"
       "google.gemini-cli-vscode-ide-companion"
     ];
-    # Enable previews
-    previews = {
-      enable = true;
-      previews = {
-        # web = {
-        #   # Example: run "npm run dev" with PORT set to IDX's defined port for previews,
-        #   # and show it in IDX's web preview panel
-        #   command = ["npm" "run" "dev"];
-        #   manager = "web";
-        #   env = {
-        #     # Environment variables to set for your server
-        #     PORT = "$PORT";
-        #   };
-        # };
-      };
-    };
-    # Workspace lifecycle hooks
-    workspace = {
-      # Runs when a workspace is first created
-      onCreate = {
-        # Example: install JS dependencies from NPM
-        # npm-install = "npm install";
-        # Open editors for the following files by default, if they exist:
-        default.openFiles = [ ".idx/dev.nix" "README.md" ];
-      };
-      # Runs when the workspace is (re)started
-      onStart = {
-        # Example: start a background task to watch and re-build backend code
-        # watch-backend = "npm run watch-backend";
-      };
-    };
+
+    # workspace lifecycle hooks.
+    # We removed the `onCreate` hook that was running `pip install`
+    # because the python packages are now managed by Nix.
+    workspace = {};
   };
 }
